b6098ed6e9e8b0142e22ba82b27574ab47efeb2a
henrique.alonso <henrique.alonso>
2025-01-25 02:11:05 -0300

    Implementação conversão

--- src/BuildingBlocks/Core.Infra.S3/S3Service.cs
+++ src/BuildingBlocks/Core.Infra.S3/S3Service.cs
@@ -1,4 +1,5 @@
 ﻿using Amazon.S3;
+using Amazon.S3.Model;
 using Amazon.S3.Transfer;
 using Microsoft.AspNetCore.Http;
 
@@ -8,28 +9,28 @@ namespace Core.Infra.S3
     {
         private const string BucketName = "amzn-s3-bucket-26bda3ac-c185-4185-a9f8-d3697a89754c-framepack";
 
-        public async Task<string> UploadFileAsync(IFormFile file, Guid userId)
+        public async Task<string> UploadArquivoAsync(Guid id, IFormFile arquivo)
         {
-            var key = $"{userId}/{file.FileName}";
+            var key = string.Concat("aguardando-processamento/", id, Path.GetExtension(arquivo.FileName).ToLowerInvariant());
 
             try
             {
-                using var newMemoryStream = new MemoryStream();
-                await file.CopyToAsync(newMemoryStream);
+                using var memoryStream = new MemoryStream();
+                await arquivo.CopyToAsync(memoryStream);
 
                 var uploadRequest = new TransferUtilityUploadRequest
                 {
-                    InputStream = newMemoryStream,
+                    InputStream = memoryStream,
                     Key = key,
                     BucketName = BucketName,
-                    ContentType = file.ContentType
+                    ContentType = arquivo.ContentType
                 };
 
                 var transferUtility = new TransferUtility(s3Client);
                 await transferUtility.UploadAsync(uploadRequest);
 
-                var exists = await DoesS3ObjectExistAsync(BucketName, key);
-                return !exists ? string.Empty : $"https://{BucketName}.s3.amazonaws.com/{key}";
+                var existente = await VerificarExistenciaArquivo(BucketName, key);
+                return !existente ? string.Empty : key;
             }
             catch (AmazonS3Exception ex)
             {
@@ -37,7 +38,64 @@ namespace Core.Infra.S3
             }
         }
 
-        private async Task<bool> DoesS3ObjectExistAsync(string bucketName, string key)
+        public async Task<string> UploadArquivoAsync(string caminhoArquivo)
+        {
+            var nomeArquivo = Path.GetFileName(caminhoArquivo);
+            var key = $"processado/{nomeArquivo}";
+
+            try
+            {
+                var uploadRequest = new TransferUtilityUploadRequest
+                {
+                    FilePath = caminhoArquivo,
+                    BucketName = BucketName,
+                    Key = key
+                };
+
+                var transferUtility = new TransferUtility(s3Client);
+                await transferUtility.UploadAsync(uploadRequest);
+
+                var existente = await VerificarExistenciaArquivo(BucketName, key);
+                return !existente ? string.Empty : key;
+            }
+            catch (AmazonS3Exception ex)
+            {
+                throw new AmazonS3Exception($"Erro ao efetuar o upload do arquivo: {ex.Message}", ex);
+            }
+        }
+
+        public string GerarPreSignedUrl(string key, int duracaoMinutos = 120)
+        {
+            var request = new GetPreSignedUrlRequest
+            {
+                BucketName = BucketName,
+                Key = key,
+                Expires = DateTime.UtcNow.AddMinutes(duracaoMinutos)
+            };
+
+            return s3Client.GetPreSignedURL(request);
+        }
+
+        public async Task DeletarArquivoAsync(string key)
+        {
+            try
+            {
+                var deleteObjectRequest = new DeleteObjectRequest
+                {
+                    BucketName = BucketName,
+                    Key = key
+                };
+
+                await s3Client.DeleteObjectAsync(deleteObjectRequest);
+                Console.WriteLine($"Arquivo deletado com sucesso.");
+            }
+            catch (AmazonS3Exception ex)
+            {
+                throw new AmazonS3Exception($"Erro ao deletar o arquivo': {ex.Message}", ex);
+            }
+        }
+
+        private async Task<bool> VerificarExistenciaArquivo(string bucketName, string key)
         {
             try
             {
