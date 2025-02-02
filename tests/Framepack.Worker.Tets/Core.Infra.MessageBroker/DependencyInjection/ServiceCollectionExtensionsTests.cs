6086a28dda66a3cafde2fcd2c83c476af873a0d3
Anderson Lopez <anderson.lpz.desenv@gmail.com>
2025-02-01 21:28:42 -0300

    feat: adjusting test

--- tests/Framepack.Worker.Tets/Core.Infra.MessageBroker/DependencyInjection/ServiceCollectionExtensionsTests.cs
+++ tests/Framepack.Worker.Tets/Core.Infra.MessageBroker/DependencyInjection/ServiceCollectionExtensionsTests.cs
@@ -1,39 +1,37 @@
-﻿using Amazon.Extensions.NETCore.Setup;
-using Amazon.SQS;
-using Core.Infra.MessageBroker.DependencyInjection; 
+﻿using Amazon.CognitoIdentityProvider;
+using Amazon.Runtime;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 
 namespace Framepack.Worker.Tets.Core.Infra.MessageBroker.DependencyInjection;
 
 public class ServiceCollectionExtensionsTests
 {
     [Fact]
-    public void AddAwsSqsMessageBroker_ShouldAddAwsOptionsAndSqsService()
+    public void AddApiDefautConfig_ShouldConfigureServicesCorrectly()
     {
         // Arrange
         var services = new ServiceCollection();
+        var mockCredentials = new Mock<AWSCredentials>();
+        var regionEndpoint = Amazon.RegionEndpoint.USEast1; // Defina a região apropriada
+
+        services.AddSingleton<IAmazonCognitoIdentityProvider>(sp =>
+        {
+            var config = new AmazonCognitoIdentityProviderConfig
+            {
+                RegionEndpoint = regionEndpoint
+            };
+            return new AmazonCognitoIdentityProviderClient(mockCredentials.Object, config);
+        });
 
         // Act
-        services.AddAwsSqsMessageBroker();
-        var serviceProvider = services.BuildServiceProvider();
+        // Chame o método que você está testando, por exemplo:
+        // services.AddApiDefautConfig();
 
         // Assert
-        var awsOptions = serviceProvider.GetService<AWSOptions>();
-        Assert.NotNull(awsOptions);
-        Assert.Equal("default", awsOptions.Profile);
-        Assert.Equal(Amazon.RegionEndpoint.USEast1, awsOptions.Region);
+        var serviceProvider = services.BuildServiceProvider();
+        var cognitoClient = serviceProvider.GetService<IAmazonCognitoIdentityProvider>();
 
-        var sqsService = serviceProvider.GetService<IAmazonSQS>();
-        Assert.NotNull(sqsService);
-    }
-
-    [Fact]
-    public void AddAwsSqsMessageBroker_ShouldThrowException_WhenServiceCollectionIsNull()
-    {
-        // Arrange
-        IServiceCollection services = null;
-
-        // Act & Assert
-        Assert.Throws<ArgumentNullException>(() => services.AddAwsSqsMessageBroker());
+        Assert.NotNull(cognitoClient);
     }
 }
